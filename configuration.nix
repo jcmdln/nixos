e19d16cb8817bc3e4fbc958e2394df48fa7e83bd
jcmdln <jcmdln@gmail.com>
2022-10-01 16:10:07 -0400

    Fix configuration errors

--- configuration.nix
+++ configuration.nix
@@ -4,19 +4,29 @@
   nix.settings.experimental-features = [ "nix-command" "flakes" ];
   nixpkgs.config.allowUnfree = true;
   imports = [ /etc/nixos/hardware-configuration.nix ];
-  inputs.nixpkgs.url = "github:nixos/nixpkgs";
 
   boot = {
-    kernelPackages = [ pkgs.linuxPackages_5_15 ];
+    kernelPackages = pkgs.linuxPackages_5_15;
     loader = {
       efi.canTouchEfiVariables = true;
       systemd-boot.enable = true;
     };
   };
 
-  environment.systemPackages = [ btrfs-progs curl git nix-prefetch-scripts tmux ];
-  fonts.fonts = [ noto-fonts noto-fonts-cjk noto-fonts-emoji ];
-  hardware.pulseaudio.enable = false;
+  environment.systemPackages = with pkgs; [
+    btrfs-progs
+    curl
+    git
+    nix-prefetch-scripts
+    tmux
+  ];
+
+  fonts.fonts = with pkgs; [
+    noto-fonts
+    noto-fonts-cjk
+    noto-fonts-emoji
+  ];
+
   networking.hostName = "nixos";
 
   programs = {
