9e37280e36f9c4af54857c98e4e8873c074c47f5
jcmdln <jcmdln@gmail.com>
2022-10-01 15:54:09 -0400

    Simplify base configuration

--- configuration.nix
+++ configuration.nix
@@ -1,110 +1,38 @@
 # SPDX-License-Identifier: ISC
 
 { config, pkgs, ... }: {
-  nix = {
-    package = pkgs.nixFlakes;
-    extraOptions = ''
-      experimental-features = nix-command flakes
-    '';
-  };
-
+  nix.settings.experimental-features = [ "nix-command" "flakes" ];
   nixpkgs.config.allowUnfree = true;
-
   imports = [ /etc/nixos/hardware-configuration.nix ];
+  inputs.nixpkgs.url = "github:nixos/nixpkgs";
 
   boot = {
-    kernelPackages = pkgs.linuxKernel.packages.linux_5_15;
-
+    kernelPackages = [ pkgs.linuxPackages_5_15 ];
     loader = {
       efi.canTouchEfiVariables = true;
       systemd-boot.enable = true;
     };
   };
 
-  environment.systemPackages = with pkgs; [
-    btrfs-progs
-    git
-    gnome.gnome-tweaks
-    libvirt
-    mg
-    nix-prefetch-scripts
-    tmux
-    vscode
-    wget
-  ];
-
-  fonts.fonts = with pkgs; [
-    noto-fonts
-    noto-fonts-cjk
-    noto-fonts-emoji
-    spleen
-  ];
-
+  environment.systemPackages = [ btrfs-progs curl git nix-prefetch-scripts tmux ];
+  fonts.fonts = [ noto-fonts noto-fonts-cjk noto-fonts-emoji ];
   hardware.pulseaudio.enable = false;
-
-  networking = {
-    hostName = "nixos";
-    useDHCP = false; # Deprecated, explicitly set to false
-  };
+  networking.hostName = "nixos";
 
   programs = {
-    dconf.enable = true;
-
     gnupg.agent = {
       enable = true;
       enableSSHSupport = true;
     };
-
     mtr.enable = true;
   };
 
   security.rtkit.enable = true;
-
-  services = {
-    # flatpak remote-add --if-not-exists flathub https://flathub.org/repo/flathub.flatpakrepo
-    flatpak.enable = true;
-    gvfs.enable = true;
-    openssh.enable = true;
-
-    pipewire = {
-      enable = true;
-      alsa = {
-        enable = true;
-        support32Bit = true;
-      };
-      jack.enable = false;
-      media-session.enable = false;
-      pulse.enable = true;
-    };
-
-    printing.enable = true;
-
-    xserver = {
-      enable = true;
-      desktopManager.gnome.enable = true;
-      displayManager.gdm.enable = true;
-    };
-
-  };
-
-  sound = {
-    enable = true;
-    mediaKeys.enable = true;
-  };
-
+  services.openssh.enable = true;
   time.timeZone = "US/Eastern";
 
-  users.users.john = {
-    createHome = true;
-    extraGroups = [ "libvirtd" "wheel" ];
-    isNormalUser = true;
-    openssh.authorizedKeys.keys = [ ];
-    uid = 1000;
-  };
-
   virtualisation = {
-    libvirtd = { enable = false; };
-
+    libvirtd.enable = false;
     podman = {
       enable = true;
       dockerCompat = true;
@@ -113,9 +41,7 @@
 
   zramSwap = {
     enable = true;
-    # Ensure we don't exceed a 4GiB swap
-    memoryMax = 1024 * 1024 * 4096;
-    # Use 25% of available system memory
-    memoryPercent = 25;
+    memoryMax = 4 * 1024 * 1024 * 1024; # 4GiB
+    memoryPercent = 40;
   };
 }
