0088cad722348f02034381caf792b6ca6360e269
jcmdln <jcmdln@gmail.com>
2022-10-12 21:25:36 -0400

    Woops, uncomment nixosConfigurations

--- flake.nix
+++ flake.nix
@@ -13,25 +13,20 @@
 
   outputs = { self, nixpkgs }: {
     nixosConfigurations = {
-      # "laptop" = nixpkgs.lib.nixosSystem {
-      #   system = "x86_64-linux";
-      #   modules = [ ./machine/workstation.nix ];
-      # };
-
-      # "server" = nixpkgs.lib.nixosSystem {
-      #   system = "x86_64-linux";
-      #   modules = [ ./machine/server.nix ];
-      # };
+      "server" = nixpkgs.lib.nixosSystem {
+        system = "x86_64-linux";
+        modules = [ ./machine/server.nix ];
+      };
 
       "vagrant" = nixpkgs.lib.nixosSystem {
         system = "x86_64-linux";
         modules = [ ./machine/vagrant.nix ];
       };
 
-      # "workstation" = nixpkgs.lib.nixosSystem {
-      #   system = "x86_64-linux";
-      #   modules = [ ./machine/workstation.nix ];
-      # };
+      "workstation" = nixpkgs.lib.nixosSystem {
+        system = "x86_64-linux";
+        modules = [ ./machine/workstation.nix ];
+      };
     };
   };
 }
