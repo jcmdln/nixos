d6c7a318d31dcc022b916ce51cf4a298fef22f48
jcmdln <jcmdln@gmail.com>
2022-10-21 21:27:13 -0400

    Document Flatpak setup + theme

--- machine/workstation.nix
+++ machine/workstation.nix
@@ -3,6 +3,8 @@
 { config, pkgs, ... }: {
   imports = [ ./server.nix ];
 
+  environment.systemPackages = with pkgs; [ libsForQt5.breeze-gtk ];
+
   fonts.fonts = with pkgs; [
     noto-fonts
     noto-fonts-cjk
@@ -12,6 +14,9 @@
   programs.dconf.enable = true;
 
   services = {
+    # Errata: Flatpak requires extra steps
+    # - flatpak remote-add --if-not-exists flathub https://flathub.org/repo/flathub.flatpakrepo
+    # - sudo flatpak override --filesystem=xdg-config/gtk-3.0
     flatpak.enable = true;
     pipewire.enable = true;
 
