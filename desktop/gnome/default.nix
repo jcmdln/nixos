9c3641102326e3bff263e890065bdcefbba996d0
jcmdln <jcmdln@gmail.com>
2022-12-23 18:42:00 -0500

    desktop/gnome: Install gnome-tweaks

--- desktop/gnome/default.nix
+++ desktop/gnome/default.nix
@@ -3,6 +3,10 @@
 { config, pkgs, ... }: {
   imports = [ ../common.nix ];
 
+  environment.systemPackages = with pkgs; [
+    gnome.gnome-tweaks
+  ];
+
   services.xserver = {
     desktopManager.gnome.enable = true;
     displayManager.gdm.enable = true;
