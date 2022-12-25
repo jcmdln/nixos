ab92654ca4428b7280580c6d6f26d25933c44b45
jcmdln <jcmdln@gmail.com>
2022-12-25 14:14:36 -0500

    Separate user configuration

--- flake.nix
+++ flake.nix
@@ -1,37 +1,30 @@
 # SPDX-License-Identifier: ISC
+
 {
   description = "NixOS by jcmdln";
 
   inputs = {
-    #home-manager.url = "github:nix-community/home-manager";
-    nixos-hardware.url = "github:nixos/nixos-hardware/master";
     nixpkgs.url = "github:nixos/nixpkgs/release-22.11";
+    nixos-hardware.url = "github:nixos/nixos-hardware/master";
+    home-manager = {
+      url = "github:nix-community/home-manager";
+      inputs.nixpkgs.follows = "nixpkgs";
+    };
   };
 
-  outputs = { self, nixos-hardware, nixpkgs }: {
+  outputs = { self, home-manager, nixos-hardware, nixpkgs }: {
     nixosConfigurations = {
-      "workstation" = nixpkgs.lib.nixosSystem {
-        system = "x86_64-linux";
-        modules = [
-          nixos-hardware.nixosModules.common-cpu-amd
-          nixos-hardware.nixosModules.common-cpu-amd-pstate
-          nixos-hardware.nixosModules.common-gpu-amd
-          ./desktop/gnome.nix
-          ./hardware/cpu/amd-kvm.nix
-          ./hardware/storage/nvme.nix
-          ./machine/workstation.nix
-        ];
-      };
-
       "laptop" = nixpkgs.lib.nixosSystem {
         system = "x86_64-linux";
-        modules = [
-          nixos-hardware.nixosModules.framework
+        modules = (with nixos-hardware.nixosModules; [
+          framework
+        ]) ++ ([
           ./desktop/gnome.nix
           ./hardware/cpu/intel-kvm.nix
           ./hardware/storage/nvme.nix
           ./machine/laptop.nix
-        ];
+          ./users/john.nix
+        ]);
       };
 
       "vagrant" = nixpkgs.lib.nixosSystem {
@@ -40,6 +33,21 @@
           ./hardware/cpu/qemu64.nix
           ./machine/vagrant.nix
         ];
+      };
+
+      "workstation" = nixpkgs.lib.nixosSystem {
+        system = "x86_64-linux";
+        modules = (with nixos-hardware.nixosModules; [
+          common-cpu-amd
+          common-cpu-amd-pstate
+          common-gpu-amd
+        ]) ++ ([
+          ./desktop/gnome.nix
+          ./hardware/cpu/amd-kvm.nix
+          ./hardware/storage/nvme.nix
+          ./machine/workstation.nix
+          ./users/john.nix
+        ]);
       };
     };
   };
---
+++ users/john.nix
@@ -0,0 +1,22 @@
+# SPDX-License-Identifier: ISC
+
+{ config, home-manager, ... }: {
+  security.sudo.extraRules = [{
+    users = [ "john" ];
+    commands = [{
+      command = "ALL";
+      options = [ "NOPASSWD" ];
+    }];
+  }];
+
+  users.users."john" = {
+    createHome = true;
+    extraGroups = [ "wheel" ];
+    isNormalUser = true;
+    openssh.authorizedKeys.keys = [
+      "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAILTO2nxnuc/nJBeMpi0rm+6PbDu9jl04576jRf7qgMpk"
+      "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIE4qKrTnTWdbNQHrvf5wdx24UuNvGkeGBHDNC6Z3d10S"
+      "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIIOtyoTumvN+Qyq+lzUbYmkjSxltderxQNv+3jGuIHlM"
+    ];
+  };
+}
